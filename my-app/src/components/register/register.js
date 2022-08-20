d3985f4cab9f58cdfc425bbb971f90ebf12abc95
Yash-Pal-Web <yash9451826087@gmail.com>
2022-08-20 16:34:47 +0530

    My-Project

---
+++ my-app/src/components/register/register.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react"
+import "./register.css"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
+
+const Register = () => {
+
+    const history = useHistory()
+
+    const [ user, setUser] = useState({
+        firstname: "",
+        lastname: "",
+        email:"",
+        password:""
+        
+    })
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+    const register = () => {
+        const { firstname,lastname, email, password } = user
+        if( firstname && lastname && email && password ){
+            axios.post("http://localhost:9002/register", user)
+            .then( res => {
+                alert(res.data.message)
+                history.push("/login")
+            })
+        } else {
+            alert("invlid input")
+        }
+        
+    }
+
+    return (
+        <div className="register">
+            {console.log("User", user)}
+            <h1>Register</h1>
+            <input type="text" name="firstname" value={user.name} placeholder="Your first Name" onChange={ handleChange }></input>
+            <input type="text" name="lastname" value={user.name} placeholder="Your last Name" onChange={ handleChange }></input>
+            <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
+            <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
+            
+            <div className="button" onClick={register} >Register</div>
+            <div>or</div>
+            <div className="button" onClick={() => history.push("/login")}>Login</div>
+        </div>
+    )
+}
+
+export default Register
